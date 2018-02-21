515133ace2a21e9e5fe14c1a7b943a7fa67b1139
Sara Frandsen <sara.frandsen@gmail.com>
2018-02-20 16:21:37 -0800

    pass bob

--- bob/bob.go
+++ bob/bob.go
@@ -5,11 +5,49 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
+import (
+	"strings"
+	"unicode"
+)
+
+func hasLetters(remark string) bool {
+	for _, r := range remark {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+func isShouting(remark string) bool {
+	if remark == strings.ToUpper(remark) && hasLetters(remark) {
+		return true
+	}
+	return false
+}
+
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	// if string.all == caps && string.last == ?
+	// if string.all == caps || string.last == !
+	// if string.last == ?
+	// if string == ""
+	// else
+
+	// refactor to use case
+
+	var reply string
+	remark = strings.TrimSpace(remark)
+	if isShouting(remark) && strings.HasSuffix(remark, "?") {
+		reply = "Calm down, I know what I'm doing!"
+	} else if isShouting(remark) {
+		reply = "Whoa, chill out!"
+	} else if strings.HasSuffix(remark, "?") {
+		reply = "Sure."
+	} else if remark == "" {
+		reply = "Fine. Be that way!"
+	} else {
+		reply = "Whatever."
+	}
+	return reply
 }
