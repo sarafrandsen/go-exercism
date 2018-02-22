3a7b772c3c2f3934c1fa7a03841c251446565f2c
Sara Frandsen <sara.frandsen@gmail.com>
2018-02-21 19:32:24 -0800

    fetch space age

---
+++ space-age/space_age.go
@@ -0,0 +1,35 @@
+package space
+
+func SecondsToEarthDays(seconds int) float64 {
+	// seconds * 60 = minutes
+	// minutes * 60 = hours
+	// hours * 24 = days
+	// days * 365.25 = Earth year
+
+}
+
+func MercurcyAge(seconds int) float64 {
+
+}
+func VenusAge(seconds int) float64 {
+
+}
+func MarsAge(seconds int) float64 {
+
+}
+func JupiterAge(seconds int) float64 {
+
+}
+func SaturnAge(seconds int) float64 {
+
+}
+func UranusAge(seconds int) float64 {
+
+}
+func NeptuneAge(seconds int) float64 {
+
+}
+
+func SpaceAge(seconds int, planet string) float64 {
+	if 
+}
