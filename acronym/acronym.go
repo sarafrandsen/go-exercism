cb584039a0e66e9fdaa04d9310efc73bdfb00a5a
Sara Frandsen <sara.frandsen@gmail.com>
2018-02-21 19:31:47 -0800

    refactor acronym to name Space byte, start pseudo-code for space age

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -6,17 +6,18 @@ import (
 )
 
 // isSpecialChar takes c with type byte and returns true if it is a special character and false if it is a letter
-func isSpecialChar(c byte) bool {
-	return !unicode.IsLetter(rune(c))
+func isSpecialChar(char byte) bool {
+	return !unicode.IsLetter(rune(char))
 }
 
 // Abbreviate will return a string a containing only the first letter of each word in the given string s.
 func Abbreviate(s string) string {
-	a := ""
+	acronym := ""
+	const Space byte = 32
 	for i, char := range s {
-		if i == 0 || (i != 0 && isSpecialChar(s[i-1]) && s[i] != 32) {
-			a += string(char)
+		if i == 0 || (i != 0 && isSpecialChar(s[i-1]) && s[i] != Space) {
+			acronym += string(char)
 		}
 	}
-	return strings.ToUpper(a)
+	return strings.ToUpper(acronym)
 }
