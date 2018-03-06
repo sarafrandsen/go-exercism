bcdf886afdc96113e0376724ded886ae2b97e674
Frandsen, Sara <sara.frandsen@nordstrom.com>
2018-03-06 10:26:34 -0800

    change spacing/line breaks on scrabble

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -7,7 +7,14 @@ import (
 // Score returns the scrabble score for a given word
 func Score(s string) int {
 	points := map[rune]int{
-		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10,
+		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
+		'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+		'D': 2, 'G': 2,
+		'B': 3, 'C': 3, 'M': 3, 'P': 3,
+		'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+		'K': 5,
+		'J': 8, 'X': 8,
+		'Q': 10, 'Z': 10,
 	}
 
 	n := 0
